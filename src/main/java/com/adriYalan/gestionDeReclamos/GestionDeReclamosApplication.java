b765c74b2b0ddfc115eb12cf414c848b4ea55213
AdrielPasik <adriel.pasik@gmail.com>
2024-09-22 18:49:20 -0300

    Hello world deleted

--- src/main/java/com/adriYalan/gestionDeReclamos/GestionDeReclamosApplication.java
+++ src/main/java/com/adriYalan/gestionDeReclamos/GestionDeReclamosApplication.java
@@ -8,7 +8,7 @@ public class GestionDeReclamosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionDeReclamosApplication.class, args);
-        System.out.println("Hello world");
+
 	}
 
 }
