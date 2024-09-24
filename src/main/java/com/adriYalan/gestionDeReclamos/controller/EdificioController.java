58caaea539166e459751f6cb4f3d26b9dd684e11
Alan Gao <1982548075@qq.com>
2024-09-24 19:55:23 -0300

    Creacion de paquete y claese Controller y exception

---
+++ src/main/java/com/adriYalan/gestionDeReclamos/controller/EdificioController.java
@@ -0,0 +1,4 @@
+package com.adriYalan.gestionDeReclamos.controller;
+
+public class EdificioController {
+}
---
+++ src/main/java/com/adriYalan/gestionDeReclamos/exception/PersonaException.java
@@ -0,0 +1,4 @@
+package com.adriYalan.gestionDeReclamos.exception;
+
+public class PersonaException {
+}
