b54562959f4d0ecfbf6db73ff8df62589702ae81
Alan Gao <1982548075@qq.com>
2024-11-20 19:24:59 -0300

    fix ahora se muestra la imagenes, acceso publico

--- src/main/java/com/adriYalan/gestionDeReclamos/service/FirebaseStorageService.java
+++ src/main/java/com/adriYalan/gestionDeReclamos/service/FirebaseStorageService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Service
@@ -20,11 +22,17 @@ public class FirebaseStorageService {
         // Obtener la referencia al bucket
         Bucket bucket = StorageClient.getInstance().bucket(BUCKET_NAME);
 
+        // Crear un nombre único para el archivo
         String fileName = "ImgReclamo_" + idReclamo + "_" + UUID.randomUUID().toString().substring(0,6);
 
+        // Subir el archivo al bucket
         Blob blob = bucket.create(fileName, file.getInputStream(), file.getContentType());
 
-        return blob.getMediaLink();
+        // Generar el enlace público (si las reglas lo permiten)
+        String publicUrl = "https://firebasestorage.googleapis.com/v0/b/" + BUCKET_NAME + "/o/" +
+                URLEncoder.encode(fileName, StandardCharsets.UTF_8) + "?alt=media";
+
+        return publicUrl;
     }
 
     public void deleteFileByLink(String fileLink) {
@@ -57,18 +65,20 @@ public class FirebaseStorageService {
     // Método auxiliar para extraer el nombre del archivo de un enlace
     private String extractFileNameFromLink(String fileLink) {
         try {
-            // Suponiendo que el enlace tiene un formato estándar como el proporcionado
-            // Ejemplo: https://storage.googleapis.com/download/storage/v1/b/<BUCKET>/o/<FILE_NAME>?<PARAMS>
+            // Suponiendo que el enlace tiene el formato correcto:
+            // Ejemplo: https://firebasestorage.googleapis.com/v0/b/<BUCKET_NAME>/o/<FILE_NAME>?alt=media
             String[] parts = fileLink.split("/o/");
             if (parts.length > 1) {
-                String fileNameWithParams = parts[1];
-                return fileNameWithParams.split("\\?")[0]; // Elimina los parámetros
+                String fileNameWithParams = parts[1].split("\\?")[0]; // Obtener solo el nombre del archivo
+                return fileNameWithParams; // El nombre del archivo
             }
         } catch (Exception e) {
             System.out.println("Error al procesar el enlace: " + e.getMessage());
         }
         return null;
     }
+
+
 
 
 
